db5a5d5dedb92f8bb99fc7422669fb38f4770161
Matheus Pavanelli <mspavanelli@gmail.com>
2021-05-23 23:24:59 -0300

    Add responsive styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
-    extend: {},
+    colors: {
+      red: 'hsl(0, 78%, 62%)',
+      cyan: 'hsl(180, 62%, 55%)',
+      orange: 'hsl(34, 97%, 64%)',
+      blue: 'hsl(212, 86%, 64%)',
+      gray: {
+        dark: 'hsl(234, 12%, 34%)',
+        regular: 'hsl(229, 6%, 66%)',
+        light: 'hsl(0, 0%, 98%)',
+      },
+    },
+    fontFamily: {
+      sans: 'Poppins',
+    },
   },
   variants: {
     extend: {},
